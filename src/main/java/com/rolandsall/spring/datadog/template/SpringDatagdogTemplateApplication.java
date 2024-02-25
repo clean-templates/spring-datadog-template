ce3f2238c085822d54b333a8b77d82c8d434aa04
RolandSall <roland.salloum00@outlook.com>
2024-02-25 07:33:07 +0200

    feat: initial project setup

---
+++ src/main/java/com/rolandsall/spring/datadog/template/SpringDatagdogTemplateApplication.java
@@ -0,0 +1,13 @@
+package com.rolandsall.spring.datadog.template;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDatagdogTemplateApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringDatagdogTemplateApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/rolandsall/spring/datadog/template/SpringDatagdogTemplateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rolandsall.spring.datadog.template;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDatagdogTemplateApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
