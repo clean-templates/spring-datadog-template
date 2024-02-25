579f88d47653ae5c6e5de0b015f9b15107d5a21d
RolandSall <roland.salloum00@outlook.com>
2024-02-25 08:03:09 +0200

    feat: setting up needed configuration in .yml

---
+++ README.md
@@ -0,0 +1,6 @@
+# Spring X Datadog
+
+This repository will showcase the use case of datadog metrics and tracing in spring boot application
+This is fairly easy due to the integration compatibility with spring boot x datadog build in the micrometer project
+
+![img.png](resources%2Fimg.png)
--- src/main/java/com/rolandsall/spring/datadog/template/SpringDatadogTemplateApplication.java
+++ src/main/java/com/rolandsall/spring/datadog/template/SpringDatadogTemplateApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringDatagdogTemplateApplication {
+public class SpringDatadogTemplateApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(SpringDatagdogTemplateApplication.class, args);
+        SpringApplication.run(SpringDatadogTemplateApplication.class, args);
     }
 
 }
